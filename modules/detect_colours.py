e2a526a9a2503782fae8840aec8116d4bfaaaff2
Sarah Yoo <gaeunyoo3626@gmail.com>
2025-09-07 13:30:50 -0400

    run linters

--- modules/detect_colours.py
+++ modules/detect_colours.py
@@ -58,7 +58,7 @@ class DetectBlue:
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
         # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img, img, mask = mask)
+        res = cv2.bitwise_and(img, img, mask=mask)
 
         # ============
         # ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -129,7 +129,7 @@ class DetectRed:
         mask = cv2.bitwise_or(mask1, mask2)
 
         # Shows the detected colour from the mask
-        res = cv2.bitwise_and(img, img, mask = mask)
+        res = cv2.bitwise_and(img, img, mask=mask)
 
         # Annotate the colour detections
         # replace the '_' parameter with the appropiate variable
